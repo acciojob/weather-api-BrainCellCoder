72bf7c6461cf8c752e1cec8ffe24bd47047731f5
BrainCellCoder <70462882+BrainCellCoder@users.noreply.github.com>
2024-07-22 10:01:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ function getWeather() {
     const apiKey = 'f63a61575aba27041c68e06fd11c9c51'; 
     const city = 'London';
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(response => {
-            document.getElementById('weatherData').textContent = `Current weather in London: ${response.weather[0].main}`;
-        })
-        .catch(error => console.error('Error fetching weather data:', error));
+    // fetch(apiUrl)
+    //     .then(response => response.json())
+    //     .then(response => {
+    //         document.getElementById('weatherData').textContent = `Current weather in London: ${response.weather[0].main}`;
+    //     })
+    //     .catch(error => console.error('Error fetching weather data:', error));
 }
